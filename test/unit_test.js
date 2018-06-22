9e102e1049b9097142dc6e8f89e06727cbc9237e
Mark Hainline <wucheeman@gmail.com>
2018-06-22 13:19:30 -0400

    first commit

---
+++ test/unit_test.js
@@ -0,0 +1,16 @@
+// @ts-check
+// This turns on type checking in VS Code
+
+// these are needed only if testing with node
+var myArray = require('../myArray');
+var chai = require('chai');
+var assert = chai.assert;
+
+describe("replace with test group description", function() {
+  it('replace with expected outcome description', function() {
+    // declare entity under test - the SUT
+    // insert test code here
+    // uncomment statement below
+    // assert.equal(<SUT variable>, expected value);
+  });
+});
