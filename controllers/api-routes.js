4a91d721787385be78983afd8fd23c43269fc10d
Mark Hainline <wucheeman@gmail.com>
2018-06-25 21:19:28 -0400

    moved routes to controllers directory

---
+++ controllers/api-routes.js
@@ -0,0 +1,89 @@
+var db = require("../models");
+var request = require("request");
+var cheerio = require("cheerio");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  app.get("/scraped", function(req, res) {
+    console.log('Starting to scrape')
+    request("http://www.heraldsun.com/news/local/community/chapel-hill-news/", function(error, response, html) {
+      var $ = cheerio.load(html);
+  
+      $("div.teaser").each(function(i, element) {
+        var link = $(element).children('.title').children('a').attr('href');
+        var title = $(element).children('.title').children('a').text();
+        title = title.trim();
+        var summary = $(element).children('.summary').text();
+        if (summary === '') {
+          summary = 'No summary available';
+        } else {
+          // removes leading and trailing line breaks
+          summary = summary.replace(/^\s+|\s+$/g, '');
+        }
+  
+        if (link !== undefined) {
+          var result = {};
+          result.title = title;
+          result.link = link;
+          result.summary = summary;
+          
+          db.Article.create(result)
+            .then(function(dbArticle) {
+              console.log(dbArticle);
+            })
+            .catch(function(err) {
+              // TODO: return status page, not error
+             return res.json(err);
+          });
+        }
+      }); // end of .each
+      res.send("Site scraped");
+    });
+  }); // end of scraping
+  
+  
+  // Route for getting all articles from db
+  app.get("/articles", function(req, res) {
+    db.Article.find({})
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // TODO: send status page instead
+        res.json(err);
+      });
+  });
+  
+  // Route for populating article with a note
+  app.get("/articles/:id", function(req, res) {
+    db.Article.findOne({ _id: req.params.id })
+      .populate("note")
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // TODO: send status page instead
+        res.json(err);
+      });
+  });
+  
+  // Route for saving/updating an Article's associated Note
+  app.post("/articles/:id", function(req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.Note.create(req.body)
+      .then(function(dbNote) {
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      })
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // TODO: send status page instead
+        res.json(err);
+      });
+  });
+
+}
+
--- server.js
+++ server.js
@@ -3,13 +3,6 @@ var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
-var request = require("request");
-// TODO: move from request to axios
-// var axios = require("axios");
-var cheerio = require("cheerio");
-
-var db = require("./models");
-
 // TODO: update to enable running on Heroku
 var PORT = 3000;
 
@@ -20,90 +13,10 @@ var app = express();
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+require("./controllers/routes.js")(app);
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/scrapegracedb");
-
-// Routes
-
-app.get("/scraped", function(req, res) {
-  console.log('Starting to scrape')
-  request("http://www.heraldsun.com/news/local/community/chapel-hill-news/", function(error, response, html) {
-    var $ = cheerio.load(html);
-
-    $("div.teaser").each(function(i, element) {
-      var link = $(element).children('.title').children('a').attr('href');
-      var title = $(element).children('.title').children('a').text();
-      title = title.trim();
-      var summary = $(element).children('.summary').text();
-      if (summary === '') {
-        summary = 'No summary available';
-      } else {
-        // removes leading and trailing line breaks
-        summary = summary.replace(/^\s+|\s+$/g, '');
-      }
-
-      if (link !== undefined) {
-        var result = {};
-        result.title = title;
-        result.link = link;
-        result.summary = summary;
-        
-        db.Article.create(result)
-          .then(function(dbArticle) {
-            console.log(dbArticle);
-          })
-          .catch(function(err) {
-            // TODO: return status page, not error
-           return res.json(err);
-        });
-      }
-    }); // end of .each
-    res.send("Site scraped");
-  });
-}); // end of scraping
-
-
-// Route for getting all articles from db
-app.get("/articles", function(req, res) {
-  db.Article.find({})
-    .then(function(dbArticle) {
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // TODO: send status page instead
-      res.json(err);
-    });
-});
-
-// Route for populating article with a note
-app.get("/articles/:id", function(req, res) {
-  db.Article.findOne({ _id: req.params.id })
-    .populate("note")
-    .then(function(dbArticle) {
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // TODO: send status page instead
-      res.json(err);
-    });
-});
-
-// Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
-  // Create a new note and pass the req.body to the entry
-  db.Note.create(req.body)
-    .then(function(dbNote) {
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-    })
-    .then(function(dbArticle) {
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // TODO: send status page instead
-      res.json(err);
-    });
-});
 
 // Start the server
 app.listen(PORT, function() {
