2da94fdb2fc90f80079e20a0eecc5e3ed736479a
Mark Hainline <wucheeman@gmail.com>
2018-06-26 06:47:47 -0400

    updated so app does not save duplicate articles

--- models/Article.js
+++ models/Article.js
@@ -3,32 +3,29 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var ArticleSchema = new Schema({
-  // `title` is required and of type String
   title: {
     type: String,
     required: true
   },
-  // `link` is required and of type String
+  // `link` is unique and prevents storing duplicate articles
   link: {
     type: String,
+    unique: true,
     required: true
   },
-  // may not have a summary
+  // article may not have a summary
   summary: {
     type: String,
     required: false
   },
-  // `note` is an object that stores a Note id
-  // The ref property links the ObjectId to the Note model
-  // This allows us to populate the Article with an associated Note
+  // `note` stores the object id of a Note
+
   note: {
     type: Schema.Types.ObjectId,
     ref: "Note"
   }
 });
 
-// This creates our model from the above schema, using mongoose's model method
 var Article = mongoose.model("Article", ArticleSchema);
 
-// Export the Article model
 module.exports = Article;
